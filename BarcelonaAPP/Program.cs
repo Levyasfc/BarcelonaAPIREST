ff7080300643aa58bf0a7d8e3ab65f7e2bc201cf
Levyasfc <santiagolzcdb@gmail.com>
2025-08-20 14:45:51 -0500

    CAmbio de estructura

--- BarcelonaAPP/Program.cs
+++ BarcelonaAPP/Program.cs
@@ -1,7 +1,37 @@
-ï»¿
-namespace BarcelonaAPP
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configura los servicios para la aplicación web
+builder.Services.AddControllersWithViews();
+
+// Registra HttpClient para consumir la API
+// Asegúrate de que esta URL coincida con la URL de tu proyecto de API
+builder.Services.AddHttpClient("ApiClient", client =>
 {
-    public class Program
-    {
-    }
+    client.BaseAddress = new Uri("https://localhost:7176/");
+});
+
+var app = builder.Build();
+
+// Configura el pipeline HTTP para la aplicación web
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ BarcelonaAPP/Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace BarcelonaAPP
+{
+    public class Startup
+    {
+    }
+}
